0a1a4ee3c99d138a4ee2810ad941ddd8d0babb6f
Erik Svensson <arizon.es@gmail.com>
2025-01-26 11:39:23 +0100

    add logging to be explicit about what kind of backing storage is used

--- cmd/sse/main.go
+++ cmd/sse/main.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 
 	"github.com/arizon-dread/sse/api"
+	"github.com/arizon-dread/sse/internal/config"
 	"github.com/arizon-dread/sse/pkg/http/middlewares/headers"
 )
 
 func main() {
-	log.Println("Starting simple Server-sent Events lab")
+	cfg := config.Get()
+	backingCacheText := "instance in-memory cache, not suitable for multi instance use"
+	if len(cfg.Cache.Url) > 0 {
+		backingCacheText = "valkey/redis cache and stream"
+	}
+	log.Printf("Starting Server-sent Events micro service api with %v\n", backingCacheText)
 	mux := http.NewServeMux()
 	events := http.HandlerFunc(api.Events)
 	mux.Handle("GET /events/{recipient}", headers.SseHeadersMiddleware(events))
